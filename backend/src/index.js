0fb8068b4ed106473629aac0e655c61fdca01283
devin567 <avinnash567@gmail.com>
2026-06-15 21:40:56 +0530

    fixed minor bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -48,10 +48,14 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  app.use((req, res, next) => {
+    if (req.path.startsWith("/api")) return next();
+
+    res.sendFile(
+      path.resolve(__dirname, "frontend", "dist", "index.html")
+    );
   });
 }
 
